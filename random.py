5224f02b9d978440247cbf58614f6bf76375a3a8
HikaruF <hikaru.furukawa.hf@gmail.com>
2023-03-15 15:34:47 -0700

    Added another code that use sample() method in random module. This generates a list of random numbers by specifying a range and how many numbers we want to generate

--- random.py
+++ random.py
@@ -6,3 +6,10 @@ print(n)
 import random
 n = random.randint(0,100)
 print(n)
+
+
+#Practice using random.sample() method 
+import random
+#Generate 5 random numbers between 0 and 100
+randomlist = random.sample(range(0, 100), 5)
+print(randomlist)
