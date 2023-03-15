1dbd57f0d0da6be9de8139cfbd22172fb6889bba
HikaruF <hikaru.furukawa.hf@gmail.com>
2023-03-15 15:37:58 -0700

    Added another code to generate a list of numbers using a for loop. Create an empty list and append the random numbers to the list

--- random.py
+++ random.py
@@ -13,3 +13,10 @@ import random
 #Generate 5 random numbers between 0 and 100
 randomlist = random.sample(range(0, 100), 5)
 print(randomlist)
+
+# Use For Loop for generating a list of numbers
+randomlist = []
+for i in range(0,5): 
+	n = random.randint(0,100)
+	randomlist.append(n)
+print(randomlist)
